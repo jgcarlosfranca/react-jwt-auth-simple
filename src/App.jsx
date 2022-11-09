7451e8c63d04a3513dae9664e9cd7f7c18de085d
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-09 17:32:43 -0300

    Criação das rotas da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Resgister from "./Views/Resgister";
+import Login from "./Views/Login";
+import Secret from "./Views/Secret";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/redister" element={<Resgister />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Secret />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
