75c3567be08e70bd2a14fc06f0a0dad037a63ec4
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-10 20:18:52 -0300

    criando primeiro o card de login

--- src/Views/Resgister.jsx
+++ src/Views/Resgister.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 function Resgister() {
   return (
-    <div>Resgister</div>
-  )
+    <div className="container">
+      <h2>Register Account</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account ?<Link to="/login"> Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default Resgister
+export default Resgister;
