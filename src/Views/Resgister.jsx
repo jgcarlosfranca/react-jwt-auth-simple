8401a0a9e486a79eb39d473884ac32e47207688e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-10 20:23:45 -0300

    setando onChange para os campos de email e pass

--- src/Views/Resgister.jsx
+++ src/Views/Resgister.jsx
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
 function Resgister() {
   const [values, setValues] = useState({ email: "", password: "" });
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5500/register",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/");
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   return (
     <div className="container">
       <h2>Register Account</h2>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div>
           <label htmlFor="email">Email</label>
           <input
             type="email"
             name="email"
             placeholder="Email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
           />
         </div>
         <div>
@@ -24,6 +51,9 @@ function Resgister() {
             type="password"
             placeholder="Password"
             name="password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
           />
         </div>
         <button type="submit">Submit</button>
